7d8d052b4309bc6318eaa2b8f6e3c8a7db8628d3
ballalssica <ssicanara@naver.com>
2024-11-26 15:18:51 +0900

    feat: 이미지피커 기능추가
    이미지피커 기능추가

--- lib/product_resistration/product_resistration.dart
+++ lib/product_resistration/product_resistration.dart
@@ -1,10 +1,29 @@
 import 'package:flutter/material.dart';
-import 'package:shoppingmall/product_resistration/widget/image_picker.dart';
+import 'dart:io'; // File 사용을 위해 필요
+import 'package:image_picker/image_picker.dart';
 import 'package:shoppingmall/product_resistration/widget/inputbox.dart';
 import 'package:shoppingmall/product_resistration/widget/resistration_button.dart';
 
-class ProductRegistration extends StatelessWidget {
+class ProductRegistration extends StatefulWidget {
   const ProductRegistration({super.key});
+
+  @override
+  _ProductRegistrationState createState() => _ProductRegistrationState();
+}
+
+class _ProductRegistrationState extends State<ProductRegistration> {
+  File? _selectedImage; // 선택된 이미지 파일 저장
+  final ImagePicker _picker = ImagePicker(); // ImagePicker 인스턴스 생성
+
+  // 이미지 선택 함수
+  Future<void> _pickImage() async {
+    final pickedFile = await _picker.pickImage(source: ImageSource.gallery); // 갤러리에서 이미지 선택
+    if (pickedFile != null) {
+      setState(() {
+        _selectedImage = File(pickedFile.path); // 선택된 이미지를 _selectedImage에 저장
+      });
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -22,7 +41,27 @@ class ProductRegistration extends StatelessWidget {
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.center,
           children: [
-            const ImagePicker(),
+            GestureDetector(
+              onTap: _pickImage, // 이미지 선택 함수 호출
+              child: Container(
+                height: 300,
+                color: Colors.blue,
+                child: _selectedImage == null
+                    ? const Center(
+                        child: Icon(
+                          Icons.add,
+                          size: 50,
+                          color: Colors.white,
+                        ),
+                      )
+                    : Image.file(
+                        _selectedImage!,
+                        fit: BoxFit.cover,
+                        width: double.infinity,
+                        height: double.infinity,
+                      ),
+              ),
+            ),
             const SizedBox(height: 25),
             InputBox(),
           ],
